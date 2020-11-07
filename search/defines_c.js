229332f2f77f1253f3e9e75684c3730fa48e4761
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-07 23:45:14 +0000

    Deploy stnolting/neorv32 to github.com/stnolting/neorv32.git:gh-pages

---
+++ search/defines_c.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['uart_5fct',['UART_CT',['../neorv32_8h.html#add80bf63566866d28e6bed5190a841b5',1,'neorv32.h']]],
+  ['uart_5fdata',['UART_DATA',['../neorv32_8h.html#ae8063fe1a693e0f849bd626b28a5447c',1,'neorv32.h']]],
+  ['use_5fasm_5fversion',['USE_ASM_VERSION',['../blink__led_2main_8c.html#af8db284ada73a95e4c7cddde5572ee19',1,'main.c']]]
+];
